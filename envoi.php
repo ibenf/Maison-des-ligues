afe7a3a4ac45d51435b453b8793926a6e10b8bba
Alexandre <loolex@hotmail.fr>
2013-12-15 20:34:12 +0100

    first commit

---
+++ envoi.php
@@ -0,0 +1,28 @@
+
+<html>
+<head>
+  <title>La maison des ligues de lorraine</title>
+<LINK rel=STYLESHEET href="styleservice.css" type="text/css">
+<body>
+
+ <BODY background="fondecran.jpg">
+
+<!-- Contenu principal -->
+<body> <bold> <FONT SIZE=5> PPE Groupe 6 - Lycée privée Charles de foucauld </bold> </font>
+
+</body>
+<div class=contenu>
+<BR>
+<img src=banniere.jpg alt=titre/>
+<BR><div class="nav">
+<tr>
+<td align="center"><CENTER> <font size=5>Vos coordonnées <br>
+ont bien été envoyées.<br>
+<br>
+Une réponse vous sera envoyée prochainement.</td> </font>
+<P><font size=2> <a href="index.html">Retour à l'index du site</a> </font>  </center>
+
+</tr>
+</table>
+</div>
+</html>
---
+++ reservation.php
@@ -0,0 +1,119 @@
+<body><form name="form1" method="post" action="envoi.php">
+<table width="580" border="0" cellspacing="0" cellpadding="0">
+<tr>
+
+<td width="7" bgcolor="#CCCCCC"> </td>
+<td width="218" bgcolor="#CCCCCC"> </td>
+<td colspan="4" bgcolor="#CCCCCC"> </td>
+<td width="20" bgcolor="#CCCCCC"> </td>
+</tr>
+<tr>
+<td width="7" bgcolor="#CCCCCC"> </td>
+<td align="center" valign="middle">Nom de l'association</td>
+<td colspan="4"><input name="nom" type="text" size="50"></td>
+<td width="20" bgcolor="#CCCCCC"> </td>
+</tr>
+<td width="7" bgcolor="#CCCCCC"> </td>
+<td align="center" valign="middle">Nom du Président de l'association</td>
+<td colspan="4"><input name="nom" type="text" size="50"></td>
+<td width="20" bgcolor="#CCCCCC"> </td>
+</tr>
+<tr>
+<td width="7" bgcolor="#CCCCCC"> </td>
+<td align="center" valign="top">Date de Reservation </td>
+<td colspan="4">Jour
+<select name="select1">
+<option>01</option>
+<option>02</option>
+<option>03</option>
+<option>04</option>
+<option>05</option>
+<option>06</option>
+<option>07</option>
+<option>08</option>
+<option>09</option>
+<option>10</option>
+<option>11</option>
+<option>12</option>
+<option>13</option>
+<option>14</option>
+<option>15</option>
+<option>16</option>
+<option>17</option>
+<option>18</option>
+<option>19</option>
+<option>20</option>
+<option>21</option>
+<option>22</option>
+<option>23</option>
+<option>24</option>
+<option>25</option>
+<option>26</option>
+<option>27</option>
+<option>28</option>
+<option>29</option>
+<option>30</option>
+<option>31</option>
+</select>
+Mois
+<select name="select2">
+<option>Janvier</option>
+<option>Février</option>
+<option>Mars</option>
+<option>Avril</option>
+<option>Mai</option>
+<option>Juin</option>
+<option>Juillet</option>
+<option>Août</option>
+<option>Septembre</option>
+<option>Octobre</option>
+<option>Novembre</option>
+<option>Décembre</option>
+</select></td>
+<td width="20" bgcolor="#CCCCCC"> </td>
+</tr>
+<tr>
+<td width="7" bgcolor="#CCCCCC"> </td>
+<td align="center" valign="top">Nombre de personnes </td>
+<td colspan="4"><input name="enfant" type="text" size="3" maxlength="2">
+</td>
+<td width="20" bgcolor="#CCCCCC"> </td>
+</tr>
+<tr>
+<td width="7" bgcolor="#CCCCCC"> </td>
+<td align="center" valign="top">Adresse email pour confirmation de la réservation </td>
+<td colspan="4"><input name="mail" type="text" size="50"></td>
+<td width="20" bgcolor="#CCCCCC"> </td>
+</tr>
+<tr>
+<td width="7" bgcolor="#CCCCCC"> </td>
+<td align="center" valign="middle">Vos messages éventuels </td>
+<td colspan="4"><textarea name="commentaire" cols="46" rows="5"></textarea></td>
+<td width="20" bgcolor="#CCCCCC"> </td>
+</tr>
+<tr>
+<td bgcolor="#CCCCCC"> </td>
+<td> </td>
+<td width="104"> </td>
+<td width="61" valign="middle"><input type="submit" value="envoyer" >
+
+</td>
+<td width="123"> </td>
+<td width="4"> </td>
+<td bgcolor="#CCCCCC"> </td>
+</tr>
+<tr>
+<td bgcolor="#CCCCCC"> </td>
+<td> </td>
+<td colspan="4"> </td>
+<td bgcolor="#CCCCCC"> </td>
+</tr>
+<tr>
+<td width="7" bgcolor="#CCCCCC"> </td>
+<td bgcolor="#CCCCCC"> </td>
+<td colspan="4" bgcolor="#CCCCCC"> </td>
+<td width="20" bgcolor="#CCCCCC"> </td>
+
+</tr>
+</table></form>
+</body>
